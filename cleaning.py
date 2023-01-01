7d2f152632ef0a3ab875183335efc9f61fa01248
Andres Crucetta Nieto <acrucettanieto@gmail.com>
2022-12-31 19:17:32 -0600

    updated article names

---
+++ cleaning.py
@@ -0,0 +1,12 @@
+import os
+
+# Set the directory where the files are located
+directory = '_posts'
+
+# Iterate through all the files in the directory
+for filename in os.listdir(directory):
+    # Split the filename on the '_' character to get the date and title
+    if '_' in filename:
+        new_filename = filename.replace('_','-')
+        # Use the os.rename function to rename the file
+        os.rename(os.path.join(directory, filename), os.path.join(directory, new_filename))
